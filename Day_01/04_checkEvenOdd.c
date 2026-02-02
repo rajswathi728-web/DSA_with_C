b37ac65e19844d5178f7e1b04f67e72d9c887039
rajswathi728-web <rajswathi728@gmail.com>
2026-02-02 11:08:07 +0530

    DSA Day 01

---
+++ Day_01/04_checkEvenOdd.c
@@ -0,0 +1,16 @@
+
+// Check Even or Odd (if-else)
+
+#include <stdio.h>
+
+int main()
+{
+    int num = 7;
+
+    if (num % 2 == 0)
+        printf("Even Number\n");
+    else
+        printf("Odd Number\n");
+
+    return 0;
+}
---
+++ Day_05/06_StringLengthWithoutstrlen.c
@@ -0,0 +1,16 @@
+
+// Find String Length Without strlen
+
+#include <stdio.h>
+
+int main() {
+    char str[] = "Hello";
+    int i = 0;
+
+    while (str[i] != '\0')
+        i++;
+
+    printf("Length = %d\n", i);
+
+    return 0;
+}
