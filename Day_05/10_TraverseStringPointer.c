67f3a709417b8290c15d4ef2577d98de9bebc58a
rajswathi728-web <rajswathi728@gmail.com>
2026-02-02 10:18:00 +0530

    06_DynamicStringmalloc

---
+++ Day_05/10_TraverseStringPointer.c
@@ -0,0 +1,18 @@
+
+// Traverse String Using Pointer ⭐
+
+#include <stdio.h>
+
+int main()
+{
+    char str[] = "World";
+    char *p = str;
+
+    while (*p != '\0')
+    {
+        printf("%c ", *p);
+        p++;
+    }
+
+    return 0;
+}
